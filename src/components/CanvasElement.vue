9958f4ea1a83653e619a4f713200a3ce28f35aea
JakubGiedzicz <jakubgiedzicz@gmail.com>
2025-03-13 08:45:58 +0100

    triangle draw fix, onupdate

--- src/components/CanvasElement.vue
+++ src/components/CanvasElement.vue
@@ -1,7 +1,6 @@
 <script setup lang="ts">
 import type ICanvasTriangle from '@/assets/types/canvas-triangle';
-import { onMounted, ref, type Ref } from 'vue';
-import { defineProps } from 'vue';
+import { onMounted, onUpdated, ref, type Ref } from 'vue';
 const emit = defineEmits(['calcTriangle'])
 const props = defineProps({
   angleA: Number,
@@ -14,21 +13,24 @@ const triangle: Ref<ICanvasTriangle> = ref({
   angleA: 0,
   angleB: 0,
   angleC: 0,
-  //sideA = C<->B
   sideA: 0,
-  //sideB = C<->A
   sideB: 0,
-  //sideC = B<->A
   sideC: 0
 })
+const getDegrees = (angle: number) => {
+  return angle*Math.PI/180
+}
 const drawTriangle = () => {
+  const h = Math.sin(getDegrees(props.angleB))*triangle.value.sideC
+  const x = Math.cos(getDegrees(props.angleB))*triangle.value.sideC 
+  console.log(Math.cos(Math.PI/180)*props.angleB)
   ctx.value?.clearRect(0, 0, 600, 400)
   calcTriangle()
   ctx.value!.strokeStyle = "#ffffff"
   ctx.value?.beginPath()
   ctx.value?.moveTo(250, 200)
   ctx.value?.lineTo(250+triangle.value.sideA, 200)
-  ctx.value?.lineTo(250, 200+ triangle.value.sideC)
+  ctx.value?.lineTo(250+x, 200+ h)
   ctx.value?.lineTo(250, 200)
   ctx.value?.stroke()
 }
@@ -36,14 +38,21 @@ const calcTriangle = () => {
   triangle.value.angleA = props.angleA
   triangle.value.angleB = props.angleB
   triangle.value.angleC = 180 - props.angleA - props.angleB
+  //between B and C
   triangle.value.sideA = props.sideA
+  //between A and C
   triangle.value.sideB = Math.sin(((Math.PI / 180) * props.angleB))/Math.sin(((Math.PI / 180) * props.angleA))*100
+  //between A and B
   triangle.value.sideC = Math.sin(((Math.PI / 180) * (180 - props.angleA - props.angleB)))/Math.sin(((Math.PI / 180) * props.angleA))*100
 }
 onMounted(() => {
   canvas.value = document.getElementById("canvas")
   ctx.value = canvas.value.getContext("2d")
   calcTriangle()
+  drawTriangle()
+})
+onUpdated(()=> {
+  drawTriangle()
 })
 </script>
 <template>
@@ -53,12 +62,12 @@ onMounted(() => {
     <p>Angle B: {{ triangle.angleB }}</p>
     <p>Angle C: {{ triangle.angleC }}</p>
 <br />
-<p>Side A: {{ triangle.sideA }}</p>
-<p>Side B: {{ triangle.sideB.toFixed(0) }}</p>
-<p>Side C: {{ triangle.sideC.toFixed(0) }}</p>
+<p>Side a: {{ triangle.sideA }}</p>
+<p>Side b: {{ triangle.sideB.toFixed(2) }}</p>
+<p>Side c: {{ triangle.sideC.toFixed(2) }}</p>
   </div>
   <div class="canvas_container">
-    <canvas width="600" height="400" id="canvas" @click="drawTriangle"></canvas>
+    <canvas width="600" height="400" id="canvas"></canvas>
   </div>
 </template>
 <style>
@@ -67,5 +76,9 @@ onMounted(() => {
   border: 2px solid green;
   cursor: pointer;
   box-shadow: 8px 8px 24px 0px rgb(12, 12, 12);
+}
+.stats {
+  margin-inline: 16px;
+  text-wrap: nowrap;
 }
 </style>
